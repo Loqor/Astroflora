e92a5767374742aab26296a3662c81cfaecd1c77
MagicMaan <38400400+Magicmaan@users.noreply.github.com>
2023-08-30 13:48:29 +0100

    oh my fucking god these comments
    added rotatable player
    fixed not having odd/weird no of sides on planets

--- game.lua
+++ game.lua
@@ -33,42 +33,44 @@ function BOOT()
   p.vy = 0
 end
 
--- L function used for drawing and rotation, also unused for some reason :)))))))) :insert clown emoji here:.
-function rspr(sx,sy,scale,angle,mx,my,mw,mh,key,useMap) --I promise I didn't steal this :( | Hello, Loqor here, and yes, MagicMan(?) did steal this from MonstersGoBoom
-  --because he (MagicMan(?)) sucks balls.
 
-  -- This is fixed, to make a textured quad.
-  -- X, Y, U, V
+
+
+-- L function used for drawing and rotation, also unused for some reason :)))))))) :insert clown emoji here:.
+function rspr(sx,sy,scale,angle,mx,my,mw,mh,key,useMap) --I promise I didn't steal this :(
+
+  --  this is fixed , to make a textured quad
+  --  X , Y , U , V
   local sv ={{-1,-1, 0,0},
              { 1,-1, 0.999,0},
              {-1,1,  0,0.999},
              { 1,1,  0.999,0.999}}
 
-	local rp = {} -- Rotated points storage.
+	local rp = {} --  rotated points storage
 
-  -- The scale is mw (map width) * 4 * scale. 
-  -- Mapwidth is * 4 because we actually want HALF width to center the image.
+  --  the scale is mw ( map width ) * 4 * scale 
+  --  mapwidth is * 4 because we actually want HALF width to center the image
   local scalex = (mw<<2) * scale
   local scaley = (mh<<2) * scale
-  -- Rotate the quad points.
+  --  rotate the quad points
   for p=1,#sv do 
-    -- Apply scaling.
+    -- apply scale
     local _sx = sv[p][1] * scalex 
 		local _sy = sv[p][2] * scaley
-    -- Apply rotations.
+    -- apply rotation
 		local a = angle
 		local rx = _sx * math.cos(a) - _sy * math.sin(a)
 		local ry = _sx * math.sin(a) + _sy * math.cos(a)
-    -- Apply transformations.
+    -- apply transform
     sv[p][1] = rx + sx
     sv[p][2] = ry + sy
-    -- Scale UV's.
+    -- scale UV's 
     sv[p][3] = (mx<<3) + (sv[p][3] * (mw<<3))
     sv[p][4] = (my<<3) + (sv[p][4] * (mh<<3))
   end
-  -- Draw two triangles for the quad.
-  textri( sv[1][1],sv[1][2], -- Do I want to explain this? Yes. Do I understand this? No. Is it necessary? Apparently so or else it wouldn't be here (sarcasm moment).
-          sv[1][1],sv[2][2],
+  -- draw two triangles for the quad
+  textri( sv[1][1],sv[1][2],
+          sv[2][1],sv[2][2],
           sv[3][1],sv[3][2],
           sv[1][3],sv[1][4],
           sv[2][3],sv[2][4],
@@ -91,7 +93,7 @@ end
 --whatever the hell this weird 2D sliced up poor planet is.
 function makePlanet(x,y,radius,sides,UVx,UVy,UVw,UVh)
 
-  local angletemp = math.rad(360//sides)
+  local angletemp = math.rad(360/sides)
   local angle = angletemp
 
   local lx = radius * math.cos(angletemp) + radius
@@ -128,7 +130,27 @@ function init()
   t=t+1
 
   -- p = player.
-  p={x=116,y=64,spr=258,ax=0.3,ay=0.3,vx=0,vy=0,dx=0.8,dy=0.8}
+  p={x=116,
+    y=64,
+    spr=258,
+    ax=0.3,
+    ay=0.3,
+    vx=0,
+    vy=0,
+    dx=0.8,
+    dy=0.8}
+
+
+  player = {
+    x=116,
+    y=64,
+    movespeed=0.5,
+    maxspeed = 2.5,
+    turnfactor = 2,
+    angle = 0,
+    velocity = 0,
+    angularvelocity = 0,
+  }
 
   -- f = temporary fire behind ship. BUG: FIRE LAGS BEHIND SHIP AS IT SLOWS DOWN.
   f={x=p.x,y=p.y + 16,sprf=290}
@@ -145,6 +167,24 @@ function init()
   speed=4.5
 end
 
+
+function newvelocity()
+  
+
+  player.angle = player.angle + player.angularvelocity
+  if player.angle > 360 then
+    player.angle = player.angle-360
+  end
+
+  player.x = player.velocity*math.cos(math.rad(player.angle)) +player.x
+
+  player.y = player.velocity*-math.sin(math.rad(player.angle)) +player.y
+
+
+  player.velocity = player.velocity /1.1
+  player.angularvelocity = player.angularvelocity / 1.1
+end
+
 -- Described in TIC() function because hehe.
 function velocity()
 
@@ -155,6 +195,9 @@ function velocity()
   --end
 
   -- Basic code that was mimicked from the velocity function in my old game (magic's old code which is kinda bad but it's okay for now).
+
+
+
 
   -- I'm not.. particularly sure why we're putting the velocity values in parenthesis, but hey, I don't really care, it's just parenthesis.
   p.x = p.x + (p.vx) -- Temporary ship's x position = Temporary ship's x position PLUS (parenthesis for some reason) Temporary ship's velocity in the x direction.
@@ -177,42 +220,22 @@ function flyShipAndMove()
 
   -- Press A or Left Arrow to move to the left. Crazy, I know.
   if btn(2) or key(01) then
-    if p.vx > -speed then
-      p.vx = p.vx - (p.ax) -- Again, the motherf**king parenthesis. WHY??
-      f.x = p.x + 16
-      f.y = p.y
-      rot = 3
-    end
+    player.angularvelocity = player.angularvelocity -1
   end
 
   -- Press D or Right Arrow to move to the right. Nuts, I'm aware.
   if btn(3) or key(04) then
-    if p.vx < speed then
-      p.vx = p.vx + (p.ax)
-      f.x = p.x - 16
-      f.y = p.y
-      rot = 1
-    end
+    player.angularvelocity = player.angularvelocity +1
   end
 
   -- Press W or Up Arrow to move to up/forward. Insane, I understand.
-  if btn(0) or key(23) then
-    if p.vy > -speed then
-      p.vy = p.vy - (p.ay)
-      f.y = p.y + 16
-      f.x = p.x
-      rot = 0
-    end
+  if btn(0) or key(23) and player.velocity > -player.maxspeed then
+    player.velocity = player.velocity - player.movespeed
   end
 
   -- Press S or Bottom Arrow to move down/backward. Crikey, I gets it.
-  if btn(1) or key(19) then
-    if p.vy < speed then
-      p.vy = p.vy + (p.ay)
-      f.y = p.y - 16
-      f.x = p.x
-      rot = 2
-    end
+  if btn(1) or key(19) and player.velocity < player.maxspeed then
+    player.velocity = player.velocity + player.movespeed
   end
 end
 
@@ -229,11 +252,32 @@ function draw()
 
   makePlanet(circlex,circley,circleradius,circlesides,8,0,8,32)
 
+
+  rspr(player.x,player.y,1,player.angle,
+      2,0,2,2,
+      00,1)
+
+
+  sprix = 1
+  spriy = 8
+  angle = 0
+    rspr( player.y,
+      player.x,          --  x,y
+      2,      --  scale
+      math.rad(player.angle),      --  angle
+      (sprix*2),  --  mapX
+      spriy*2,              --  mapY
+      2,              --  mapWidth
+      2,              --  mapHeight
+      0,false)
+
+      --rspr(sx,sy,scale,angle,mx,my,mw,mh,key,useMap)
+
   -- Draws the flames from behind the temporary ship
-  spr(f.sprf,(f.x-8)-cam.x,(f.y-8)-cam.y, 0, 1, 0 , rot, 2, 2)
+  --spr(f.sprf,(f.x-8)-cam.x,(f.y-8)-cam.y, 0, 1, 0 , rot, 2, 2)
 
   -- Draws the sprite for said temporary ship, is centered as well.
-  spr(p.spr,(p.x-8)-cam.x,(p.y-8)-cam.y, 0, 1, 0 , rot, 2, 2)
+  --spr(p.spr,(p.x-8)-cam.x,(p.y-8)-cam.y, 0, 1, 0 , rot, 2, 2)
 
   -- Draws the sprite for the mouse cursor.
   spr(4,gmouse().x,gmouse().y,0,1)
@@ -255,7 +299,7 @@ function TIC()
     flyShipAndMove()
 
     -- Refer to velocity() function comments.
-    velocity()
+    newvelocity()
 
     -- There were a lot of issues getting this shit to work properly, for instance, when I fixed it, the poke was the wrong
     --type of poke; e.g., "poke4(0x03FF8, 0)" when it should just be poke(). Plus, it was placed *after* the vbank(1) selection call,
@@ -288,10 +332,11 @@ function TIC()
     draw()
 
     -- Testing, draws the values of the temporary ship's position on screen: warning, uses fixed point numbers because it's goofy.
-    print(p.x, 32, 32)
-    print(p.y, 32, 40)
-    print("x: ", 20, 32)
-    print("y: ", 20, 40)
+    --print(p.x, 32, 32)
+    --print(p.y, 32, 40)
+    print("x: "..player.x, 20, 32)
+    print("y: "..player.y, 20, 40)
+    print("angle: "..player.angle,20,48)
 
     -- Exit the current game to console with "`"
     if key(44) then
@@ -310,19 +355,23 @@ end
 
 -- <TILES>
 -- 001:4444444444444444a44aa44aa4aaa4aaaaaaaa2aa22aa2222222aa22aaa22a22
--- 002:0000055500005555000222550002225500022259000222590222555b0222555b
--- 003:55555000555555505555552022222220999999001199110011bb1100bbbbbb00
+-- 002:6666655566665555666222556662225566622259666222596222555b6222555b
+-- 003:55555666555555565555552622222226999999661199116611bb1166bbbbbb66
 -- 004:0000000000059000000000000202505005029020000000000009500000000000
 -- 016:5558855555888855555885555558855555588555555885555558855555588555
 -- 017:2222aaaa2aa22a22aaaa222aaaa222aaa222a2aa22aaa22a2aaaaa222aaaaaa2
--- 018:022225550222bbb80000bbee00000bee00000111000001110000311100003330
--- 019:bbbbb00088880000448800004444bb004444ee00011100000133000000330000
+-- 018:622225556222bbb86666bbee66666bee66666111666661116666311166663330
+-- 019:bbbbb66688886666448866664444bb664444ee66011166660133666600336666
 -- 033:abaaaaaabaaaabaabaabaabababbaabbbbbbbbbbbbbbbbbbeeeeeeeeeeeeeeee
 -- 034:0000000000000fff000eeeee00eeebbb00bbbbbb0eeeeeee0bbbbbbb022aaaab
 -- 035:00000000fff00000eeeee000bbbeee00bbbbbb00eeeeeee0ebbbbbe0bb222bb0
 -- 049:eeeeeeeeeeeeeeeeffffffffffffffffffffffffffffffffffffffffffffffff
 -- 050:0aaaaa220bbbbbbb0eeeeeee00bbbbbb00bbbbbb000beeee00000fff00000000
 -- 051:2223aaa0bbaaabb0ebbbbbe0eeeeee00bbbbbb00eeeeb000fff0000000000000
+-- 096:0000055500005555000222550002225500022259000222590222555b0222555b
+-- 097:55555000555555505555552022222220999999001199110011bb1100bbbbbb00
+-- 112:022225550222bbb80000bbee00000bee00000111000001110000311100003330
+-- 113:bbbbb00088880000448800004444bb004444ee00011100000133000000330000
 -- 224:000000000000000000000000000000009999999999888999ded444d44d444ded
 -- </TILES>
 
